48a304b77c26202808bd768d458bde90cf7858b8
Aleksey Averianov <flex_d1@mail.ru>
2022-11-06 17:34:39 +0300

    Version 1.0.1

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,37 @@ const gulp = require('gulp')
 const stylus = require('gulp-stylus')
 const rename = require('gulp-rename')
 const cleanCSS = require('gulp-clean-css')
-const babel = require('gulp-babel')
 const uglify = require("gulp-uglify")
 const concat = require('gulp-concat')
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin')
+const newer = require('gulp-newer')
+const fileinclude = require('gulp-file-include');
+const browserSync = require('browser-sync').create();
 const del = require('del')
+
 
 const paths = {
     styles: {
-        src: 'src/css/**/*.styl',
+        src: 'src/css/*.styl',
         dest: 'dist/css/'
     },
     scripts: {
         src: 'src/scripts/**/*.js',
         dest: 'dist/js/'
+    },
+    images: {
+        src: 'src/images/**',
+        dest: 'dist/images/'
+    },
+    html: {
+        src: 'src/*.html',
+        watcher: 'src/**/*.html',
+        dest: 'dist'
+    },
+    fonts: {
+        src: 'src/fonts/*',
+        dest: 'dist/fonts/'
     }
 }
 
@@ -23,19 +41,34 @@ const paths = {
 
 // очищаем папку dist
 function clean() {
-    return del(['dist'])
+    return del(['dist/*', '!dist/images']) // очищаем все, кроме папки images
+}
+
+//Задача для html
+
+function html() {
+    return gulp.src(paths.html.src)
+    .pipe(fileinclude())
+    .pipe(gulp.dest(paths.html.dest)) // сохраняем в папку указываем куда это будет сохраняться
+    .pipe(browserSync.stream());
 }
 
 // Задача для стилей
 function styles() {
     return gulp.src(paths.styles.src)
         .pipe(stylus()) // подключаем препроцессор Stylus
-        .pipe(cleanCSS()) // очищаем старый css
+        .pipe(autoprefixer({ // префиксы для разных браузеров
+			cascade: false
+		})) 
+        .pipe(cleanCSS({
+            level: 2,
+        })) // очищаем старый css
         .pipe(rename({ // переименовываем в другое название файл
             basename: 'main',
             suffix: '.min'
         }))
         .pipe(gulp.dest(paths.styles.dest)) // сохраняем в папку указываем куда это будет сохраняться
+        .pipe(browserSync.stream());
 }
 
 //Задача для js
@@ -43,24 +76,54 @@ function scripts() {
     return gulp.src(paths.scripts.src, {
         sourcemaps: true,
     })
-    .pipe(babel()) // плагин для использования нового стандарта в старых версиях
     .pipe(uglify()) // плагин для минификации
     .pipe(concat('main.min.js'))
     .pipe(gulp.dest(paths.scripts.dest)) // сохраняем в папку указываем куда это будет сохраняться
+    .pipe(browserSync.stream());
+}
+
+// Задача для изображение
+
+function img() {
+    return gulp.src(paths.images.src)
+    .pipe(newer(paths.images.dest))
+    .pipe(imagemin({
+        progressive: true,
+    }))
+    .pipe(gulp.dest(paths.images.dest))
+}
+
+// Задача для Шрифтов
+
+function fonts() {
+    return gulp.src(paths.fonts.src)
+    .pipe(gulp.dest(paths.fonts.dest))
 }
 
 // Отслеживание изменений
 function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './dist/' // указываем в каком месте у нас стартовая дирректория для html
+        },
+    });
+    gulp.watch(paths.html.watcher).on('change', browserSync.reload) // отслеживать html при изменении
     gulp.watch(paths.styles.src, styles) // указываем где отслеживаем изменения и какую задачу выполняем
     gulp.watch(paths.scripts.src, scripts) // указываем где отслеживаем изменения и какую задачу выполняем
+    gulp.watch(paths.images.src, img) // указываем где отслеживаем изменения и какую задачу выполняем
+    gulp.watch(paths.html.watcher, html) // указываем где отслеживаем изменения и какую задачу выполняем
+    gulp.watch(paths.fonts.src, fonts) // указываем где отслеживаем изменения и какую задачу выполняем
 }
 
-const build = gulp.series(clean, gulp.parallel(styles, scripts ), watch) // выполнение сложной задачи друг за другом
+const build = gulp.series(clean, gulp.parallel(html,fonts, styles, scripts, img), watch) // Финальный билд
 
 
 exports.clean = clean
+exports.img = img
 exports.styles = styles
 exports.scripts = scripts
 exports.watch = watch
+exports.html = html
+exports.fonts = fonts
 exports.build = build
 exports.default = build // запуск задачи по умолчанию "gulp"
